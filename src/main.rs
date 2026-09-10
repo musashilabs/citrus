6441fdb1a9b978c84fd3644968eda64e53240457
Rohit <rohitnarnolia88@gmail.com>
2026-09-11 00:36:55 +0530

    chore: log file names

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 use std::path::Path;
 use notify::{Event, RecursiveMode, Result, Watcher};
 use std::sync::mpsc;
+use notify::event::CreateKind::{File, Folder};
+use notify::EventKind::Create;
 
-
-fn main()-> Result<()> {
+fn main() -> Result<()> {
     let (tx,rx) = mpsc::channel::<Result<Event>>();
 
     let mut watcher = notify::recommended_watcher(tx)?;
 
-    watcher.watch(&Path::new("."), RecursiveMode::Recursive)?;
+    watcher.watch(&Path::new("."), RecursiveMode::NonRecursive)?;
 
     for res in rx {
         match res {
-            Ok(val) => println!("Found this: {:?}", val),
+            Ok(event) =>{
+                if event.kind == Create(File) || event.kind == Create(Folder){
+                    let path = event.paths.last().unwrap();
+                    println!("Created: {:?}", path.file_name().unwrap().to_string_lossy());
+                }
+            }
             Err(e) => eprintln!("Failed with error: {}", e)
         }
     }
