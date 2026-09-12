eb6863129e54b4d79b01bdd91621d916dcb50e86
Rohit <rohitnarnolia88@gmail.com>
2026-09-12 19:08:00 +0530

    style: rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-use std::path::Path;
-use notify::{Event, RecursiveMode, Result, Watcher};
-use std::sync::mpsc;
-use notify::event::CreateKind::{File, Folder};
-use notify::EventKind::Create;
 use citrus::types::{FileMapper, FileType};
+use notify::{
+    Event,
+    EventKind::Create,
+    RecursiveMode, Result, Watcher,
+    event::CreateKind::{File, Folder},
+};
+use std::path::Path;
+use std::sync::mpsc;
 
 fn main() -> Result<()> {
-
     let mut file_mapper = FileMapper::new();
-    let (tx,rx) = mpsc::channel::<Result<Event>>();
+    let (tx, rx) = mpsc::channel::<Result<Event>>();
 
     let mut watcher = notify::recommended_watcher(tx)?;
 
@@ -16,27 +18,29 @@ fn main() -> Result<()> {
 
     for res in rx {
         match res {
-            Ok(event) =>{
-                if event.kind == Create(File) || event.kind == Create(Folder){
+            Ok(event) => {
+                if event.kind == Create(File) || event.kind == Create(Folder) {
                     let path = event.paths.last().unwrap();
 
                     dbg!("Created: {:?}", path.file_name().unwrap().to_string_lossy());
 
                     let filename = path.file_name().unwrap().to_string_lossy().to_string();
-                    match path.extension().unwrap().to_str().unwrap() {
-                        ".jpg"|".png"|".gif"|".jpeg"|".heif" => {
+                    let extension = path.extension().unwrap().to_str().unwrap();
+
+                    match extension {
+                        ".jpg" | ".png" | ".gif" | ".jpeg" | ".heif" => {
                             file_mapper.records.push(FileType::Image(filename))
                         }
-                        ".pdf"|".docx"|".txt"| "xlsx" => {
+                        ".pdf" | ".docx" | ".txt" | "xlsx" => {
                             file_mapper.records.push(FileType::Doc(filename))
                         }
-                        ".zip"|".tar"|".rar" => {
+                        ".zip" | ".tar" | ".rar" => {
                             file_mapper.records.push(FileType::Archive(filename))
                         }
-                        ".mkv"|".mp4"|"mov" => {
+                        ".mkv" | ".mp4" | "mov" => {
                             file_mapper.records.push(FileType::Video(filename))
                         }
-                        ".mp3"|".wav"|".mp4a" => {
+                        ".mp3" | ".wav" | ".mp4a" => {
                             file_mapper.records.push(FileType::Audio(filename))
                         }
                         _ => {
@@ -45,8 +49,7 @@ fn main() -> Result<()> {
                     }
                 }
             }
-            Err(e) => eprintln!("Failed with error:  {}", e)
-
+            Err(e) => eprintln!("Failed with error:  {}", e),
         }
     }
 
